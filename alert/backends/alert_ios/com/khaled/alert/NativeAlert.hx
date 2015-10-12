d83b0f90be5b853978ce7940f320a0764353470f
Khaledgarbaya <khaledgarbaya@gmail.com>
2015-10-12 17:04:55 +0200

    iOS backend

--- alert/backends/alert_ios/com/khaled/alert/NativeAlert.hx
+++ alert/backends/alert_ios/com/khaled/alert/NativeAlert.hx
@@ -1,11 +1,15 @@
 package com.khaled.alert;
+import cpp.Lib;
 class NativeAlert
 {
     /**
         Native Alert in ios
     **/
+    /// link to function in the native side
+    public static alertNative = Lib.load("alert_ios", "alert_ios_alert_native", 1);
     public static function alert(message: String = ""): Void
     {
-        
+        /// call the native function
+        alertNative(message);
     }
 }
--- alert/backends/alert_ios/project/src/ExternalInterface.mm
+++ alert/backends/alert_ios/project/src/ExternalInterface.mm
@@ -6,14 +6,24 @@
 #include <Foundation/Foundation.h>
 
 
-static value alert_native(value hxString)
+static value alert_ios_alert_native(value hxString)
 {
-    NSString *string = [NSString stringWithCString:val_get_string(hxString) encoding:NSUTF8StringEncoding];
-    // do alert native
+    /// convert value to NSString
+    NSString *messageString = [NSString stringWithCString:val_get_string(hxString) encoding:NSUTF8StringEncoding];
+
+    // do alert native using UIAlertView
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Native ios Alert"
+                                                  message:messageString
+                                                 delegate:nil
+                                        cancelButtonTitle:@"OK"
+                                        otherButtonTitles:nil];
+    [alert show];
+    [alert release];
+
 	return alloc_null();
 }
 
-DEFINE_PRIM(alert_native, 1);
+DEFINE_PRIM(alert_ios_alert_native, 1);
 
 
 extern "C" int alert_ios_register_prims() { return 0; }
