1c57d85521fb28c4736f430db25f1b397bc869f3
Khaledgarbaya <khaledgarbaya@gmail.com>
2015-10-12 17:04:41 +0200

    android backend

--- alert/backends/alert_android/com/khaled/alert/NativeAlert.hx
+++ alert/backends/alert_android/com/khaled/alert/NativeAlert.hx
@@ -1,11 +1,15 @@
 package com.khaled.alert;
+import hxjni.JNI;
 class NativeAlert
 {
     /**
         Native Alert in android
     **/
+    private static var alertNative = JNI.createStaticMethod("com/khaled/alertnative/ExternalInterface",
+    "alert", "(Ljava/lang/String;)V");
+
     public static function alert(message: String = ""): Void
     {
-
+        alertNative(message);
     }
 }
--- alert/backends/alert_android/java/com/khaled/nativealert/ExternalInterface.java
+++ alert/backends/alert_android/java/com/khaled/nativealert/ExternalInterface.java
@@ -1,9 +1,20 @@
 package com.khaled.nativealert;
+/// duell activity is created by the duell build android plugin
+import org.haxe.duell.DuellActivity;
 
 public class ExternalInterface
 {
-    public static void alert(String string)
+    public static void alert(String messageString)
     {
-        // do alert native
+        AlertDialog alertDialog = new AlertDialog.Builder(DuellActivity.getInstance().this).create();
+        alertDialog.setTitle("Android Alert");
+        alertDialog.setMessage(messageString);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+        new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
